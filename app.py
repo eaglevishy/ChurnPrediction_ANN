3abc8e802c0fbdeac23c24009bc543ff68ae5116
eaglevishy <srinivasagurdle481@gmail.com>
2024-11-12 20:03:33 +0530

    app.py

--- app.py
+++ app.py
@@ -61,6 +61,8 @@ input_data_scaled = scaler.transform(input_data)
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
+st.write(f'Churn Probability: {prediction_proba: .2f}')
+
 if prediction_proba > 0.5:
     st.write('likely to churn')
 else:
